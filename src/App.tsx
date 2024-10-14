d6f727346108b732f4053bd0b089fb75f55d4545
rishit-singh <rsrishitsingh@gmail.com>
2024-10-13 18:44:50 -0700

    Added audio recording

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { useSpeechRecognition } from './hooks/useSpeechRecognition'
+import { useSpeechRecognition } from './hooks/useSpeechRecognition';
+import { useVoiceRecorder } from './hooks/useVoiceRecorder';
 
 function App() {
   const [transcriptOutput, setTranscriptOutput] = useState<string>('');
@@ -12,13 +13,31 @@ function App() {
     setTranscriptOutput(transcriptOutput + result);
   } });
 
+  const { isRecording, audioURL, startRecording, stopRecording } = useVoiceRecorder();
+
+  if (error) {
+    alert(error);
+  }
+
+  const start = async () => {
+    startListening();
+    startRecording();
+  }
+
+  const stop = async () => {
+    stopListening();
+    stopRecording();
+  }
+
   return (
     <>
       <h1>YapNote</h1>
       <div>
-        <button onClick={(isListening) ? stopListening : startListening} className={`${isListening ? "bg-gray-500" : "bg-blue-500"} text-white p-2 rounded-md`}>{isListening ? 'Listening...' : 'Start Listening'}</button>
+        <button onClick={(isListening) ? stop  : start} className={`${isListening ? "bg-gray-500" : "bg-blue-500"} text-white p-2 rounded-md`}>{isListening ? 'Listening...' : 'Start Listening'}</button>
       </div>
       <p>{transcript}</p>
+      {(!isRecording && audioURL) && <audio src={audioURL?.toString()} controls />}
+      {(!isRecording) && <a href={audioURL?.toString()} download>Download</a>}
     </>
   )
 }
---
+++ src/hooks/useVoiceRecorder.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState, useRef } from "react";
+
+export const useVoiceRecorder = () => {
+    const [isRecording, setIsRecording] = useState(false);
+    const [audioBlob, setAudioBlob] = useState<Blob | null>(null);
+    const [audioURL, setAudioURL] = useState<string | null>(null);
+    
+    const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+
+    const startRecording = async () => {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        mediaRecorderRef.current = new MediaRecorder(stream);
+        mediaRecorderRef.current.ondataavailable = (event) => {
+            if (event.data.size > 0) {
+                setAudioBlob(event.data);
+            }
+        };
+        setIsRecording(true);
+        mediaRecorderRef.current.start();
+    }
+
+    const stopRecording = () => {
+        if (mediaRecorderRef.current) {
+            setIsRecording(false);
+            mediaRecorderRef.current.stop();
+        }
+    }
+
+    useEffect(() => {
+        if (audioBlob) {
+            setAudioURL(URL.createObjectURL(audioBlob));
+        }
+    }, [audioBlob]);      
+
+    return { isRecording, audioURL, startRecording, stopRecording };
+}       
